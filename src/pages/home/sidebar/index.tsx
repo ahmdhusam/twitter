c97914b9dab133dcc5fc6fe46230cea26a6c22c8
ahmdhusam <ahmdhusam7@gmail.com>
2022-05-24 23:46:26 +0200

    init

--- src/pages/home/sidebar/index.tsx
+++ src/pages/home/sidebar/index.tsx
@@ -1,5 +1,44 @@
+import { Link } from 'react-router-dom';
+
 import './index.scss';
 
+const footerLinks = [
+    'Terms of Service',
+    'Privacy Policy',
+    'Cookie Policy',
+    'Accessibility',
+    'Ads info',
+    'More'
+];
+
 export default function SideBar() {
-    return <aside id='sidebar'>SideBar</aside>;
+    return (
+        <aside id='sidebar'>
+            <input
+                className='search rounded'
+                type='text'
+                placeholder='Search Twitter'
+            />
+            <div className='rounded'>
+                <h5 className='title'>What’s happening</h5>
+            </div>
+            <div className='rounded'>
+                <h5 className='title'>Who to follow</h5>
+            </div>
+            <ul>
+                {footerLinks.map(hrf => (
+                    <li key={hrf.trim().replaceAll(' ', '-').toLowerCase()}>
+                        <Link
+                            to={
+                                '/' +
+                                hrf.trim().replaceAll(' ', '-').toLowerCase()
+                            }>
+                            {hrf === 'More' ? `${hrf}...` : hrf}
+                        </Link>
+                    </li>
+                ))}
+                <li>© 2022 Twitter, Inc.</li>
+            </ul>
+        </aside>
+    );
 }
